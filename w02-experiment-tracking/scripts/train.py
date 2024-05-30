23e80437fbcea07dfed07a317b08bd003054c87d
fitri <fitri.indw@gmail.com>
2024-05-30 11:46:48 +0700

    homework Q3

--- w02-experiment-tracking/scripts/train.py
+++ w02-experiment-tracking/scripts/train.py
@@ -3,7 +3,8 @@ import pickle
 import click
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+import mlflow
 
 
 def load_pickle(filename: str):
@@ -19,14 +20,28 @@ def load_pickle(filename: str):
 )
 def run_train(data_path: str):
 
+    print("running training", data_path)
+    #add mlflow experiment tracking code
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("green-taxi-experiment")
+
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    #add mlflow experiment tracking autologging
+    mlflow.autolog(
+        log_model_signatures=False,
+        extra_tags={"developer": "Fitri Indra"},
+    )
+    with mlflow.start_run():
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+
+        print("rmse", rmse)
 
 
 if __name__ == '__main__':
